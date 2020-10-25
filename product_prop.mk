Xtended-Devices/device_xiaomi_phoenix

phoenix: Add more camera props

product_prop.mk
@@ -32,7 +32,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
-    vendor.camera.aux.packagelist=org.lineageos.snap,net.sourceforge.opencamera,org.codeaurora.snapcam
+    persist.camera.gyro.disable=0 \
+    persist.camera.privapp.list=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
+    persist.vendor.camera.privapp.list=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
 
 # Recorder
 PRODUCT_PRODUCT_PROPERTIES += \

Abhinavgupta371/device_xiaomi_phoenix-2

product_prop.mk
@@ -14,7 +14,10 @@ endif
 
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
-    vendor.camera.aux.packagelist=org.lineageos.snap,net.sourceforge.opencamera,org.codeaurora.snapcam
+    persist.camera.gyro.disable=0 \
+    persist.camera.privapp.list=org.codeaurora.snapcam,com.android.camera,com.google.camera \
+    persist.vendor.camera.privapp.list=org.codeaurora.snapcam,com.android.camera,com.google.camera \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera
 
 # Recorder
 PRODUCT_PRODUCT_PROPERTIES += \